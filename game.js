1890c46cf6d1846778e54dab7e0e507e28637b87
mrodrigues <mrodrigues.uff@gmail.com>
2017-07-29 15:49:41 -0300

    Show logo

--- game.js
+++ game.js
@@ -11,9 +11,12 @@ window.onload = function() {
     });
 
     function preload () {
+        game.load.image('logo', 'node_modules/phaser/phaser-logo-small.png');
     }
 
     function create () {
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
     }
 
     function update() {
