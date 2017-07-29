930dfdd530c500810afcc538ef2b8ca98b67d17c
Luana Perin <luanakperin@gmail.com>
2017-07-29 17:10:33 -0300

    Grid criation done

--- game.js
+++ game.js
@@ -11,13 +11,33 @@ window.onload = function() {
     });
 
     function preload () {
-        game.load.image('logo', 'node_modules/phaser/phaser-logo-small.png');
+
+        game.load.image('wall', 'images/wall.png');
+        game.load.image('path', 'images/path.png');
     }
 
     function create () {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+      var grid = [[0,1],[1,0],[0,1],[0,1],[0,1]];
+
+      var x = 0;
+      var y = 0;
+
+      grid.forEach(function(row) {
+        row.forEach(function(cell) {
+          if (cell == 0) {
+            game.add.sprite(x, y, 'wall');
+          } else {
+            game.add.sprite(x, y, 'path');
+          }
+
+          x += 50;
+        });
+
+        x = 0
+        y += 50;
+      });
     }
+
 
     function update() {
     }
